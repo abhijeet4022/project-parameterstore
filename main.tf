e08d83b1ee65019ef2a6eceb5fbd8b952cbe3495
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-11-21 08:48:34 +0530

    Create Encrypted Parameters and Load Runner VM

--- main.tf
+++ main.tf
@@ -4,5 +4,6 @@ resource "aws_ssm_parameter" "foo" {
   name     = each.key
   type     = each.value["type"]
   value    = each.value["value"]
+  key_id   = "arn:aws:kms:us-east-1:060795929502:key/44d09148-0677-4a8b-a183-cd2a9b03b2eb"
 }
 
